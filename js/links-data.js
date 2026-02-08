010374e729ccced28397e6389f57e0e0f887b1c2
Scaveng3r <128704976+Scaveng3rZero@users.noreply.github.com>
2026-02-08 12:39:14 -0500

    Update links-data.js

--- js/links-data.js
+++ js/links-data.js
@@ -14,6 +14,14 @@ var GAME_LINKS = [
   { title: "Leap of Faith", platform: "steam", url: "https://store.steampowered.com/app/1768640/Leap_of_Faith/" },
   { title: "Quickie: Fantasy Adventure", platform: "itch", url: "https://oppaigames.itch.io/quickie-fantasy-adventure" },
   { title: "Race of Life", platform: "steam", url: "https://store.steampowered.com/app/2454570/Race_of_Life__Act_1/" },
+  { title: "Chasing Tails", platform: "steam", url: "https://store.steampowered.com/app/1555110/Chasing_Tails_A_Promise_in_the_Snow/" },
+  { title: "City of Broken Dreamers", platform: "steam", url: "https://store.steampowered.com/app/1358250/City_of_Broken_Dreamers_Book_One/" },
+  { title: "Cockwork Industries Complete", platform: "steam", url: "https://store.steampowered.com/app/1172940/Cockwork_Industries_Complete/" },
+  { title: "College Kings", platform: "steam", url: "https://store.steampowered.com/app/1463120/College_Kings__The_Complete_Season/" },
+  { title: "College Kings 2", platform: "steam", url: "https://store.steampowered.com/app/1924480/College_Kings_2__Episode_1/" },
+  { title: "Dreamland", platform: "steam", url: "https://store.steampowered.com/app/2176560/Dreamland/" },
+  { title: "Femboy Futa House", platform: "steam", url: "https://store.steampowered.com/app/3602290/FEMBOY_FUTA_HOUSE/" },
+  { title: "Fetish Locator", platform: "steam", url: "https://store.steampowered.com/app/1360980/Fetish_Locator_Week_One/" },
   { title: "Ripples", platform: "itch", url: "https://jestur.itch.io/ripples" }
 ];
 
