defec7c505565f95fbaadd0120c2aaf81306de3b
Scaveng3r <128704976+Scaveng3rZero@users.noreply.github.com>
2026-02-08 17:33:11 -0500

    Update received-data.js

--- js/received-data.js
+++ js/received-data.js
@@ -1,1 +1,8 @@
+// js/received-data.js
+// Add new items to the TOP so the newest shows first.
+
+var RECEIVED_RECS = [
+  { title: "Champion of Darkness", when: "Feb 8, 2026" }
+  // { title: "Game Name", when: "Feb 6, 2026" }
+];
 
