a61f7bf8e8a96fbbb20afdc722e954aae2d8ef70
Scaveng3r <128704976+Scaveng3rZero@users.noreply.github.com>
2026-04-06 21:43:18 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@
   // Edit these
   var VNS = [
     { title: "Fresh Women Season 2", episode: 20, stage: 4 },
-    { title: "Eternum", episode: 70, stage: 1 },
+    { title: "Eternum", episode: 70, stage: 2 },
     { title: "Race of Life", episode: 13, stage: 0 },
     { title: "Ripples", episode: 15, stage: 1 },
     { title: "Chasing Sunsets: Chapter 10", episode: 3, stage: 0 },
